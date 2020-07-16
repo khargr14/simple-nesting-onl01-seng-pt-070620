af07745314c54f71184621b315010866b679a002
Keesha Hargrove <kharg14@gmail.com>
2020-07-16 23:36:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -145,11 +145,11 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"
+          :languages => ["C"]
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  programmer_hash
+  programmer_hash 
 
 end
 
